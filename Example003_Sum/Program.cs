4a2ec312565a7469c80770c12705890d30a61810
Yana Demidovich <demyanvo1990@gmail.com>
2022-07-04 22:32:23 +0300

    Добавили рандомные числа и консоль, которая показывает их

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 30;
-int numberB = 5;
+﻿int numberA = new Random().Next(1, 10); // т.е. целые числа от 1,2,3... до 9 включительно
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
